950e5fb1029813a9eba85946b939bf68ad6bf6a3
hemangithombre1997@gmail.com <hemangithombre1997@gmail.com>
2023-12-20 12:15:53 +0530

    Initial Commit

---
+++ src/com/lead/git/LeadTest.java
@@ -0,0 +1,10 @@
+package com.lead.git;
+
+public class LeadTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Lead Git Project");
+	}
+
+}
